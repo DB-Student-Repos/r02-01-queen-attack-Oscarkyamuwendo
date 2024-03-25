cbfd72a81081ecb717226c23c7f166ac3b69db91
Oscar Kyamuwendo <148862985+Oscarkyamuwendo@users.noreply.github.com>
2024-03-25 10:37:55 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,31 @@
 #[derive(Debug)]
-pub struct ChessPosition;
+pub struct ChessPosition {
+    rank: i32,
+    file: i32,
+}
 
 #[derive(Debug)]
-pub struct Queen;
+pub struct Queen(ChessPosition);
 
 impl ChessPosition {
     pub fn new(rank: i32, file: i32) -> Option<Self> {
-        unimplemented!(
-            "Construct a ChessPosition struct, given the following rank, file: ({rank}, {file}). If the position is invalid return None."
-        );
+        if rank >= 0 && rank < 8 && file >= 0 && file < 8 {
+            Some(ChessPosition { rank, file })
+        } else {
+            None
+        }
     }
 }
 
 impl Queen {
     pub fn new(position: ChessPosition) -> Self {
-        unimplemented!("Given the chess position {position:?}, construct a Queen struct.");
+        Queen { position }
     }
 
     pub fn can_attack(&self, other: &Queen) -> bool {
-        unimplemented!("Determine if this Queen can attack the other Queen {other:?}");
+        let delta_rank = (self.position.rank - other.position.rank).abs();
+        let delta_file = (self.position.file - other.position.file).abs();
+        delta_rank == 0 || delta_file == 0 || delta_rank == delta_file
     }
+}
 }
